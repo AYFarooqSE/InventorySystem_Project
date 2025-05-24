6ab7279c2a72052b2361ef966ffb56581e64dc82
asfTech <asfyar123@gmail.com>
2025-05-24 12:26:37 +0500

    Added Fluent Validations

--- InventorySystem_API/ModelsValidators/CustomersValidation.cs
+++ InventorySystem_API/ModelsValidators/CustomersValidation.cs
@@ -1,6 +1,21 @@
-﻿namespace InventorySystem_API.ModelsValidators
+﻿using FluentValidation;
+using FluentValidation.Validators;
+using InventorySystem_API.DTOs;
+
+namespace InventorySystem_API.ModelsValidators
 {
-    public class CustomersValidation
+    public class CustomersValidation:AbstractValidator<CustomerDto>
     {
+        public CustomersValidation()
+        {
+            RuleFor(x => x.FirstName).NotEmpty().WithMessage("First Name Cannot be Empty");
+            RuleFor(x => x.FirstName).MaximumLength(20).WithMessage("First Name Character must be below 20");
+
+
+            RuleFor(x => x.LastName).NotEmpty().WithMessage("Last Name Cannot be Empty");
+            RuleFor(x => x.LastName).MaximumLength(20).WithMessage("Last Name Character must be below 20");
+
+
+        }
     }
 }
